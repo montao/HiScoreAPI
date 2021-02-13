0e4612afd994a4a0c404f55bc1b71e57e64a2224
Niklas Rosencrantz <niklasro@gmail.com>
2021-02-13 06:09:32 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,75 @@
+
+"""Demo of a RESTful API server in Python, using only the standard library.
+
+The API here is extremely simple:
+
+ *  a POST request stores a JSON object (from the POST body) at the given path
+ *  a GET request returns a JSON object stored at the given path
+
+Example usage:
+
+    # Save some JSON to /my/test
+    curl -X POST http://127.0.0.1:8080/my/test --data '{"test": 1}'
+
+    # Retrieve the saved JSON data
+    curl http://127.0.0.1:8080/my/test
+
+"""
+
+import http.server
+import json
+import traceback
+
+class LocalData(object):
+  records = {}
+
+class ApiRequestHandler(http.server.BaseHTTPRequestHandler):
+    """Handle a single HTTP request to the server."""
+
+    def do_GET(self):
+        print({self.path})
+        try:
+            data = self.server.storage.get(self.path.split("/")[1])
+            if data is None:
+                self.send_response(404, 'Not Found')
+                self.send_header('Content-Type', 'application/json; charset=utf-8')
+                self.end_headers()
+                self.wfile.write(b'{}')
+                return
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'application/json; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        except:
+            traceback.print_exc()
+
+    def do_POST(self):
+        try:
+            length = int(self.headers['Content-Length'])
+            charset = self.headers.get_content_charset('ascii')
+            data = json.loads(self.rfile.read(length).decode(charset))
+            self.server.storage[self.path.split("/")[1]] = data
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'{}')
+        except:
+            traceback.print_exc()
+
+class ApiServer(http.server.HTTPServer):
+    """HTTP server for API requests."""
+
+    def __init__(self, host, port):
+        super().__init__((host, port), ApiRequestHandler)
+        # Create the dict that will hold the objects we store and retrieve.
+        self.storage = {}
+
+
+def main():
+    """Main program."""
+    server = ApiServer('127.0.0.1', 8080)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
