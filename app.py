c761144b97323d83d9ac02b24125208402f9b895
Niklas Rosencrantz <niklasro@gmail.com>
2021-02-13 06:28:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,22 @@ import http.server
 import json
 import traceback
 
-class LocalData(object):
-  records = {}
 
 class ApiRequestHandler(http.server.BaseHTTPRequestHandler):
     """Handle a single HTTP request to the server."""
 
+
     def do_GET(self):
         print({self.path})
+        if self.path.split("/")[2] == "login":
+            print("login")
+            data = self.path.split("/")[1]
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'application/json; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+            return
+        
         try:
             data = self.server.storage.get(self.path.split("/")[1])
             if data is None:
@@ -48,6 +56,14 @@ class ApiRequestHandler(http.server.BaseHTTPRequestHandler):
             length = int(self.headers['Content-Length'])
             charset = self.headers.get_content_charset('ascii')
             data = json.loads(self.rfile.read(length).decode(charset))
+            sessionkey = self.path.split("sessionkey")[1][1:]
+            
+            if sessionkey not in self.server.records:
+                # did not log in
+                self.server.records[sessionkey] = str(userid)
+            else:
+                userid = self.server.records.get(sessionkey) 
+            print(f"userid:{userid}")
             self.server.storage[self.path.split("/")[1]] = data
             self.send_response(200, 'OK')
             self.send_header('Content-Type', 'application/json')
@@ -62,7 +78,9 @@ class ApiServer(http.server.HTTPServer):
     def __init__(self, host, port):
         super().__init__((host, port), ApiRequestHandler)
         # Create the dict that will hold the objects we store and retrieve.
-        self.storage = {}
+        self.storage = {}        
+        self.records = {"2600": "42"} # dummy user to populate with something
+
 
 
 def main():
